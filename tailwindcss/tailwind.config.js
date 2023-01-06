2790522f0278d3d83fef3e28ac10c179484aaf5a
Peyman (ATH) Naderi <peymanath@gmail.com>
2023-01-06 13:42:13 -0800

    added header panel

--- tailwindcss/tailwind.config.js
+++ tailwindcss/tailwind.config.js
@@ -1,13 +1,27 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    '../src/*.html',
-    '../public/js/*.js'
+    "../**/*.html",
+    "../public/js/*.js"
   ],
   theme: {
+    fontFamily:{
+      sans:["'Vazirmatn'"],
+      serif:["'Vazirmatn'"],
+      mono:["'Vazirmatn'"],
+      display:["'Vazirmatn'"],
+      body:["'Vazirmatn'"],
+    },
     extend: {
+      container: {
+        center: true,
+        padding: '1rem'
+      },
       colors: {
         gray: '#1F2D3D',
         primary: '#76EE59',
+        headerborder: '#C4C4C4',
+        input: '#CED4DA',
       }
     },
   },
