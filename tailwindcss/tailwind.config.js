dd935df3c077bd9188b0c8b2c8737e32d3f814e7
Peyman (ATH) Naderi <peymanath@gmail.com>
2023-01-06 12:37:09 -0800

    config tailwind

---
+++ tailwindcss/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    '../src/*.html',
+    '../public/js/*.js'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gray: '#1F2D3D',
+        primary: '#76EE59',
+      }
+    },
+  },
+  plugins: [],
+}
