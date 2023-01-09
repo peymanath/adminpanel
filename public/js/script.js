9486187d816b803789c39524841e6b2f43a3b383
Peyman (ATH) Naderi <peymanath@gmail.com>
2023-01-09 03:28:15 -0800

    added mobile menu & change sidebar desktop

--- public/js/script.js
+++ public/js/script.js
@@ -12,6 +12,9 @@ const $ = q => (d.querySelectorAll(q).length > 1) ? d.querySelectorAll(q) : d.qu
 
 // Toggle click Menu Btn
 $('.btn-menu').addEventListener('click', () => $('.sidebar-menu').classList.toggle('active'))
+$('.btn-menu-mobile').forEach(item => {
+    item.addEventListener('click', () => $('.ShowMenuMobile').classList.toggle('hidden'))
+});
 
 // product Transportation Fee toggle btn
 $('#product-fixed-fare').addEventListener('click', () => $('.productTransportationFee').classList.toggle('hidden'))
